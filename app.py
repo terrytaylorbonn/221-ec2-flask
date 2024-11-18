bf97ebc3d155b378b94a2a89f1114808970168e9
terrytaylorbonn <terrytaylorbonn@gmail.com>
2024-11-18 14:33:33 -0500

    Update app.py
    24.1118 branch1 modified on github xxxxxx

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world2():
-    return "Hello, Flask! tt 24.1117"
+    return "Hello, Flask! tt 24.1118 branch1 modified on github"
 
 if __name__ == '__main__':
     app.run(debug=True)
